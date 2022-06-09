de1f46195ed7397b4ca4d4a097298586b4f25e89
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-06-09 18:29:08 -0300

    adição de funcionalidade (excluir tarefas)

--- logica.js
+++ logica.js
@@ -1,16 +1,20 @@
 let main = document.getElementById('AreaItens');
 let input = document.getElementById('campoentrada');
+let contador = 0;
 
 function adicionar(){
-   let novoItem = `<div class="tarefa">
+   if ((input.value !== "") && (input.value !== null) && (input.value !== undefined)){
+   
+
+    let novoItem = `<div id="${contador}" class="tarefa">
    <div style="text-align:left" class="textoTarefa">
    <p>${input.value}</p>
        
        
    </div>
 
-   <div class="Deletar">
-       <button class="deletar">Deletar</button>
+   <div  class="Deletar">
+       <button onclick="deletar(${contador})" class="deletar">Deletar</button>
    </div>
    </div>
 `;
@@ -18,9 +22,15 @@ function adicionar(){
 main.innerHTML += novoItem;
 input.value = "";
 input.focus();
-
+++contador;
+   }
 }
 
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+
+}
 
 
 
