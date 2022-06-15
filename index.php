c68fc419f57c8fa391a813c44fa48dc4fa9ccfe7
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-06-15 17:00:14 -0300

    alteracoes

--- index.php
+++ index.php
@@ -1,60 +1,6 @@
 <?php 
 
+include("index.html");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gabriela Ramires | Portfólio</title>
-    <meta name="description" content="Site desenvolvido por Gabriela Ramires para portifólio profissional.">
-    <meta name="keyword content=" Gabriela Ramires, Portfólio profissional, portfólio, portfólio desenvolvedor de
-        software,portfólio programador>
-    <meta name="author" content="Gabriela Ramires">
-
-    <link rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
-
-    <style>
-        *{
-            margin:0;
-        }
-    </style>
-
-</head>
-    <div class="containerTotal">
-    <div class="menu">
-    <ul>
-        <li><a href="index.html">Inicio</a></li>
-        <li><a href="../portfolio/portfolio.html">Portfólio</a></li>
-        <li><a href="../contato/contato.html">Contato</a></li>
-    </ul>
-    </div>
-
-
-
-
-<body>
-
-<div class="apresentacao">
-    <div class="perfil">
-        <img  src="../imagens/perfil.png" width="80%" height="100%">
-    </div>
-    <div id="text" class="minhaDescricao">
-    
-    </div>
-    
-    
-    
-</div>
-
-</div>
-<footer class="footer">Desenvolvido por: Gabriela Ramires | engenheira.ramires@gmail.com</footer>
-<script src="index.js"></script>
-</body>
-
-</html>
