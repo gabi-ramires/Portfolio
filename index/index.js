514fc7d045d547773e63c2da04626f9231ae58b0
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-06-15 14:54:46 -0300

    mudanças no layout

---
+++ index/index.js
@@ -0,0 +1,16 @@
+var string = "Olá, me chamo Gabriela, tenho 25 anos e estudo programação. Gosto de aprender tecnologias e linguagens referentes ao desenvolvimento web.  \n\rPara visualizar meus projetos, clique em 'Portfólio'."; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+  if (array.length > 0) {
+    document.getElementById("text").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+      }
+  loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+
+}
+frameLooper();
+
+
